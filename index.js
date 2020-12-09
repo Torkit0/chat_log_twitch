84d863e6c8c3880c95fdd247308c639f8aba29eb
Torkit <43708528+Torkit0@users.noreply.github.com>
2020-12-09 17:51:36 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const tmi = require("tmi.js");
 require("dotenv").config();
-var fetch = require("node-fetch");
-var fs = require("fs");
-var { query } = require("express");
-var express = require("express");
-var { username } = require("tmi.js/lib/utils");
-var colors = require("colors");
-var opts = require('./opts.json')
+const fetch = require("node-fetch");
+const fs = require("fs");
+const { query } = require("express");
+const express = require("express");
+const { username, channel } = require("tmi.js/lib/utils");
+const colors = require("colors");
+const opts = require('./opts.json'); // Requires the JSON file in which the channels are saved, when being restarted or such.
+const { Console } = require("console");
 
 // Create a client with our options
 const client = new tmi.client(opts);
@@ -17,41 +18,39 @@ client.on("connected", onConnectedHandler);
 
 client.connect(); // Connect to Twitch:
 
-console.log(opts.channels[0]);
+function torkit(nameTV){ // Checks if the username that posted the message is tor_kit
+  return nameTV === "tor_kit";
+};
 
 // Called every time a message comes in
 function onMessageHandler (target, tags, msg, self) {
-  if (self) { return; } // Ignore messages from the bot
-
- //console.log(tags.['_id']);   
-  fetch(`https://api.twitch.tv/kraken/users?login=${tags.username}`, {
-    headers: {
-      "Accept": "application/vnd.twitchtv.v5+json",
-      "Client-ID": "cclk5hafv1i7lksfauerry4w7ythu2"
-    }
-  })
-  .then((response) => response.json())
-  .then((resultT) => {
+  if (self) { return; } // Ignores messages from the bot
       var dateNow = new Date();
       const time = `${dateNow.getHours()}:${dateNow.getMinutes()}:${dateNow.getSeconds()}`
       const date = `${dateNow.getDate()}-${dateNow.getMonth()}-${dateNow.getFullYear()}`
       const fileName = `${target}-${date}.log`
       const dir = `./${target}/${fileName}`
       try{
-        if(!fs.existsSync(dir)){
+        if(!fs.existsSync(dir)){ // Checks if a folder and file exists for the streamer, if it does not then it creates one
             fs.mkdir(target, (err) => {
               if (err) throw err;
             })
             console.log(`Directory [${dir}] created!`.bgGreen.black);
-            fs.writeFileSync(dir, `${target}\n#Started logging at [${date} ${time}]\n`)
-            fs.appendFileSync(dir, `\n[${time}] ${tags.username}: ${msg}`);
+            fs.writeFileSync(dir, `${target}\n#Started logging at [${date} ${time}]\n`) // Writes the first line
+            fs.appendFileSync(dir, `\n[${time}] ${tags.username}: ${msg}`); // Appends the message as to the .log file
         } else {
-          fs.appendFileSync(dir, `\n[${time}] ${tags.username}: ${msg}`);
+          fs.appendFileSync(dir, `\n[${time}] ${tags.username}: ${msg}`); // If file exists it appends the message that was sent
         }
       } catch(err){
           if (err) console.log(err)
       }
-})
+}
+  if (msg.includes("#log")){
+    if(torkit(tags.username)){ // Checks if the user is authorized to add a channel
+        opts.channels.push(`#${msg.slice(5)}`); // Adds the channel specified to the JSON
+        fs.writeFileSync("opts.json", JSON.stringify(opts))
+        client.join(`#${msg.slice(5)}`); // Joins the channel
+      }
 };
 // Called every time the bot connects to Twitch chat
 function onConnectedHandler (addr, port) {
