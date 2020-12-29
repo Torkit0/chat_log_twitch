74bdc43cf7d9c5c904b4976c9f79220478ecc593
Torkit <43708528+Torkit0@users.noreply.github.com>
2020-12-29 01:47:38 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const fetch = require("node-fetch");
 const fs = require("fs");
 const { query } = require("express");
 const express = require("express");
-const { username, channel } = require("tmi.js/lib/utils");
 const colors = require("colors");
 const opts = require('./opts.json'); // Requires the JSON file in which the channels are saved, when being restarted or such.
 const { Console } = require("console");
@@ -24,41 +23,67 @@ function checkName(nameTV){ // Checks if the username that posted the message is
 // Called every time a message comes in
 function onMessageHandler (target, tags, msg, self) {
   if (self) { return; } // Ignores messages from the bot
+      var badges = []
       var dateNow = new Date();
-      const time = `${dateNow.getHours()}:${dateNow.getMinutes()}:${dateNow.getSeconds()}`
-      const date = `${dateNow.getDate()}-${dateNow.getMonth() + 1}-${dateNow.getFullYear()}`
-      const fileName = `${date}.log`
-      const dir = `./${target}/${fileName}`
-      const folder = `${target}`
+      var streamWHashtag = target.replace("#", "")
+      var time = `${dateNow.getHours()}:${dateNow.getMinutes()}:${dateNow.getSeconds()}`
+      var date = `${dateNow.getDate()}-${dateNow.getMonth() + 1}-${dateNow.getFullYear()}`
+      var fileName = `${date}.log`
+      var dir = `./${target}/${fileName}`
+      var folder = `${target}`
+
+      if (tags.badges){
+        if (tags.badges.broadcaster){ badges.push("broadcaster") }
+        if (tags.badges.partner){ badges.push("partner") }
+        if (tags.mod){ badges.push("mod") }
+        if (tags.badges.founder){ badges.push("founder") }
+        if (tags.badges.subscriber){ badges.push(tags["badge-info"].subscriber+"m_sub") }
+        if (tags.badges.bits){ badges.push(tags.badges.bits+"_bits") }
+        if (tags.badges["bits-charity"]){ badges.push("bits-charity") }
+        if (tags.badges.premium){ badges.push("prime") }
+        if (tags.turbo){ badges.push("turbo") }
+        if (tags.badges.predictions){ badges.push(tags.badges.predictions) }
+        if (tags.badges["sub-gifter"]){ badges.push(tags.badges["sub-gifter"]+"_gifted") }
+        if (tags.badges["glhf-pledge"]){ badges.push("glhf-pledge") }
+        if (tags.badges.glitchcon2020){ badges.push("glitchCon2020") }
+        var msgAppend = `\n[${time}] [${badges}] ${tags.username}: ${msg}`
+        }
+      if (!badges.length > 0){
+        var msgAppend = `\n[${time}] ${tags.username}: ${msg}`
+      }
       try{
         if(!fs.existsSync(folder, function(err){
-          if (err) throw err;
+          if (err) console.log(err);
         })){ // Checks if a folder exists for the streamer, if it does not then it creates one
-            fs.mkdir(target, (err) => {
+            fs.mkdir(target, function(err) {
               if (err) throw err;
             })
             console.log(`Directory [${dir}] created!`.bgGreen.black);
-            fs.writeFile(dir, `${target}\n#Started logging at [${date} ${time}]\n`, function(err){
+            fs.writeFile(dir, `#First message logged at [${date} ${time}] in ${target}\n`, (err) => {
               if (err) throw err;
             }) // Writes the first line
-            fs.appendFileSync(dir, `\n[${time}] ${tags.username}: ${msg}`); // Appends the message as to the .log file
+            fs.appendFileSync(dir, msgAppend); // Appends the message to the .log file
         } else {
           if (!fs.existsSync(dir, function(err){
             if (err) throw err;
           })){
-            fs.writeFileSync(dir, `${target}\n#Started logging at [${date} ${time}]\n`, function(err){
+            fs.writeFile(dir, `#First message logged at [${date} ${time}] in ${target}\n`, (err) => {
               if (err) throw err;
             }) // Writes the first line
-            fs.appendFileSync(dir, `\n[${time}] ${tags.username}: ${msg}`); // If file exists it appends the message that was sent
+            fs.appendFileSync(dir, msgAppend, (err) => {
+              if (err) throw err;
+            }); // If file exists it appends the message that was sent
           } else {
-            fs.appendFileSync(dir, `\n[${time}] ${tags.username}: ${msg}`); // If file exists it appends the message that was sent
+            fs.appendFileSync(dir, msgAppend, (err) => {
+              if (err) throw err;
+            }); // If file exists it appends the message that was sent
           }
         }
       } catch(err){
           if (err) console.log(err)
       }
   if (msg.includes("#log")){
-    const msgSlice = msg.slice(5)
+    var msgSlice = msg.slice(5)
     if(checkName(tags.username)){ // Checks if the user is authorized to add a channel
       if (!msgSlice.includes("#")){
         opts.channels.push(`#${msgSlice}`); // Adds the channel specified to the JSON
